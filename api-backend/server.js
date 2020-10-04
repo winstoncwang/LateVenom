4328a9b4eaa2d93d17d3dfc4a4709046b1dce79d
Cong Wang <congwang@live.co.uk>
2020-10-04 12:02:53 +0100

    mongoose db connection

--- api-backend/server.js
+++ api-backend/server.js
@@ -10,6 +10,22 @@ app.use(cors());
 app.use(userRoutes);
 app.use(express.json());
 
+//mongoose connection
+const mongoose = require("mongoose");
+const options = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+mongoose.connection.connect(process.env.DB_URL, options);
+mongoose.connection.on("error", () => {
+  console.log("connection error");
+});
+mongoose.connection.once("open", () => {
+  console.log("db connection successful");
+});
+
 const PORT = process.env.PORT || process.env.LOCAL_PORT;
 
 app.listen(PORT, () => {
