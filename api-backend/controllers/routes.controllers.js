d66e660a325488c80b2e6eb873bca87b2e926beb
Cong Wang <congwang@live.co.uk>
2020-10-04 11:08:33 +0100

    setup api endpoint for login and registration

--- api-backend/controllers/routes.controllers.js
+++ api-backend/controllers/routes.controllers.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const Users = require("../model/users.model");
 
-/* sanitize using mongo-sanitize 
-and apply using a middleware 
-function cleanBody(req, res, next) {
+cleanBody = (req, res, next) => {
   req.body = sanitize(req.body);
   next();
-}
-*/
+};
 
 const Users = require("../model/users.model");
 
-router.get("/", (req, res) => {
-  res.send("hello world");
+//login
+router.post("/users", cleanBody, async (req, res) => {
+  try {
+    const result = await Users.find({ username: req.body.username });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//create account
+router.post("/users", async (req, res) => {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      res.status(500).json({
+        StatusCode: 500,
+        Message: "Empty request body, request must be made in JSON format.",
+      });
+    }
+    const newUser = new Users(req.body);
+    const result = await newUser.save();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
