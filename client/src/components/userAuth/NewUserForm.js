61cdea3be97d73aa1852520828fb19d79d32b395
Cong Wang <congwang@live.co.uk>
2020-10-16 19:48:38 +0100

    onblur change and onchange function for quick database validation of valid new user

--- client/src/components/userAuth/NewUserForm.js
+++ client/src/components/userAuth/NewUserForm.js
@@ -1,47 +1,83 @@
 import React from "react";
 import axios from 'axios';
+import InputError from './InputError'
 
 class NewUserForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       form:{
-              username: "",
-              password: "",
-              firstname: "",
-              lastname: "",
-              email: "",
-              address: "",
-              phonenumber: "",
-            },
+        username: "",
+        password: "",
+        firstname: "",
+        lastname: "",
+        email: "",
+        address: "",
+        phonenumber: "",
+      },
       isSubmitting:false,
-      validationError:{},
-      messageOpen:false
+      validationError:{
+        username: "",
+        password: "",
+        firstname: "",
+        lastname: "",
+        email: "",
+        address: "",
+        phonenumber: "",
+      },
+      messageOpen:false,
+      userExist:false
     };
   }
 
   componentDidUpdate=()=>{  }
 
-  handleInput = (e) => {
+  handleOnChange = (e) => {
     let form = {...this.state.form}
     form[e.target.name]=e.target.value;
     this.setState({form})
+
+    //validate existing username
+    if(e.target.name==='username' && e.target.value.length>0){
+      try{
+        const data = await axios.get(`http://localhost:5000/users/${e.target.value}`)
+        if(data){
+        this.setState({userExist:true})
+        }
+      }catch(err){
+        throw new Error (err)
+      }
+    }
   };
 
-  handleSubmit = async(e) => {
+  //onBlur validation
+  handleOnBlur = (e)=>{
+        //client validation
+    if(e.target.value.length===0){
+      let validationError = {...this.state.validationError}
+      validationError[e.target.name]=`${e.target.name} can not be empty`
+      this.setState({validationError:validationError})
+    }else{
+      let validationError = {...this.state.validationError}
+      validationError[e.target.name]="";
+      this.setState({validationError})
+    }
+    console.log(this.state.validationError)
+  }
+
+  handleOnSubmit = async(e) => {
     e.preventDefault();
-    console.log('submit button clicked.')
-    console.log(this.state.form)
     //AJAX call
+
     try{
     const data = await axios.post('http://localhost:5000/users',this.state.form);
-    console.log(data)
+    console.log("data: ",data)
     }catch(err){
       if(err.response.data){
       this.setState({validationError:err.response.data})
       this.setState({messageOpen:true})
       }
-      console.log(this.state.validationError,this.state.messageOpen)
+      console.log(err,this.state.validationError,this.state.messageOpen)
     }
 
   };
@@ -55,40 +91,44 @@ class NewUserForm extends React.Component {
         <form
           className="ui form"
           onSubmit={
-            this.handleSubmit
+            this.handleOnSubmit
           }
         >
           <h4 className="ui dividing header">User Information</h4>
           <div className="required field">
             <label>Username</label>
-            <input type="text" name="username" placeholder="Username" value={this.state.form.username} onChange={this.handleInput} />
-            <div className="ui pointing red basic label">
-              That name is taken!
-            </div>
+            <input type="text" name="username" placeholder="Username" value={this.state.form.username} onChange={this.handleOnChange} onBlur={this.handleOnBlur} />
+            <InputError error={this.state.validationError.username}/>
           </div>
           <div className="required field">
             <label>Password</label>
-            <input type="password" name="password" placeholder="Password" value={this.state.form.password} onChange={this.handleInput}/>
+            <input type="password" name="password" placeholder="Password" value={this.state.form.password} onChange={this.handleOnChange} onBlur={this.handleOnBlur}/>
+            <InputError error={this.state.validationError.password}/>
           </div>
           <div className="required field">
             <label>First name</label>
-            <input type="text" name="firstname" placeholder="First name" value={this.state.form.firstname} onChange={this.handleInput}/>
+            <input type="text" name="firstname" placeholder="First name" value={this.state.form.firstname} onChange={this.handleOnChange} onBlur={this.handleOnBlur}/>
+            <InputError error={this.state.validationError.firstname}/>
           </div>
           <div className="required field">
             <label>Last name</label>
-            <input type="text" name="lastname" placeholder="Last name" value={this.state.form.lastname} onChange={this.handleInput}/>
+            <input type="text" name="lastname" placeholder="Last name" value={this.state.form.lastname} onChange={this.handleOnChange} onBlur={this.handleOnBlur}/>
+            <InputError error={this.state.validationError.lastname}/>
           </div>
           <div className="required field">
             <label>E-mail</label>
-            <input type="text" name="email" placeholder="E-mail" value={this.state.form.email} onChange={this.handleInput}/>
+            <input type="text" name="email" placeholder="E-mail" value={this.state.form.email} onChange={this.handleOnChange} onBlur={this.handleOnBlur}/>
+            <InputError error={this.state.validationError.email}/>
           </div>
           <div className="required field">
             <label>Address</label>
-            <input type="text" name="address" placeholder="Address" value={this.state.form.address} onChange={this.handleInput}/>
+            <input type="text" name="address" placeholder="Address" value={this.state.form.address} onChange={this.handleOnChange} onBlur={this.handleOnBlur}/>
+            <InputError error={this.state.validationError.address}/>
           </div>
           <div className="required field">
             <label>Phone number</label>
-            <input type="text" name="phonenumber" placeholder="Phone number" value={this.state.form.phonenumber} onChange={this.handleInput}/>
+            <input type="text" name="phonenumber" placeholder="Phone number" value={this.state.form.phonenumber} onChange={this.handleOnChange} onBlur={this.handleOnBlur}/>
+            <InputError error={this.state.validationError.phonenumber}/>
           </div>
 
           <div className={`ui negative message ${ this.state.messageOpen ? '':'hidden'}`}>
